64601e0cd2c8eed81734c8e980a3880ba52ebb50
anantabhishray <104260528+anantabhishray@users.noreply.github.com>
2022-06-17 17:21:04 +0530

    Abhishray's OOPS Assignment

---
+++ oopsAssignment/src/com/oopsAssignment/SuperDepartment.java
@@ -0,0 +1,23 @@
+package com.oopsAssignment;
+
+public class SuperDepartment {
+
+	public String departmentName() {
+		String departmentName = "Super Department";
+		return departmentName;		
+	}
+	public String getTodaysWork() {
+		String todayswork = "No Work as of now";
+		return todayswork;
+	}
+	public String getWorkDeadline() {
+		String getWorkDeadline = "Nil";
+		return getWorkDeadline;
+	}
+	public String isTodayAHoliday() {
+		String isTodayAHoliday = "Today is not a Holiday";
+		return isTodayAHoliday;
+	}
+}
+
+
---
+++ oopsAssignment/src/com/oopsAssignment/TechDepartment.java
@@ -0,0 +1,20 @@
+package com.oopsAssignment;
+
+public class TechDepartment extends SuperDepartment {
+	public String departmentName() {
+		String departmentName = "Tech Department";
+		return departmentName;
+	}
+	public String getTodaysWork() {
+		String getTodaysWork = "Complete coding of module 1";
+		return getTodaysWork;
+	}
+	public String getWorkDeadline() {
+		String getWorkDeadline = "Complete by EOD";
+		return getWorkDeadline;
+	}
+	public String getTechStackInformation() {
+		String getTechStackInformation = "Core Java";
+		return getTechStackInformation;
+	}
+}
